a2083016c0bd1bb5e1466f553a11389457bfd1ea
Vinnie <vinnie1000shsw@gmail.com>
2021-11-04 00:21:59 +0100

    Initial commit

--- src/Server/MainServer.java
+++ src/Server/MainServer.java
@@ -1,4 +1,7 @@
 package Server;
 
 public class MainServer {
+    public static void main(String[] args){
+        new Server();
+    }
 }
--- src/Server/Server.java
+++ src/Server/Server.java
@@ -5,7 +5,8 @@ import java.util.*;
 public class Server {
     public Server(){
         Random rn = new Random();
-        int chaine = rn.
+        int chaine = rn.nextInt(3) + 1;
+        System.out.println(chaine);
 
     }
 }
